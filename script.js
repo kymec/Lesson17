74f7f444514a9df42cac3c3e4575e60d3e5f85cd
kymec <kummaz@ukr.net>
2019-04-03 19:55:32 +0300

    ready

---
+++ script.js
@@ -0,0 +1,23 @@
+//1)
+console.log(3*60*60);
+//2)
+var num = 1;
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num += 1;
+num -= 1;
+alert(num);
+//3)
+var a=10, b=2, c=5;
+alert(a+b+c);
+//4)
+var a = 17, b = 10;
+var c = a - b;
+var d = 7;
+var result = c + d;
+alert (result);
+//5)
+var name = 'Денис';
+alert('Привет, '+name);
